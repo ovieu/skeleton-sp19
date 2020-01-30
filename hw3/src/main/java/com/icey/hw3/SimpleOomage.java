27e6f5ed2db6d6c5c3fbd74c487e4fd94f2ffca4
ovie umukoro <ovie.u.umukoro@gmail.com>
2020-01-30 13:01:54 -0500

    Implement oomage hashcode function

--- hw3/src/main/java/com/icey/hw3/SimpleOomage.java
+++ hw3/src/main/java/com/icey/hw3/SimpleOomage.java
@@ -10,7 +10,7 @@ public class SimpleOomage implements Oomage {
     protected int blue;
 
     private static final double WIDTH = 0.01;
-    private static final boolean USE_PERFECT_HASH = false;
+    private static final boolean USE_PERFECT_HASH = true;
 
     @Override
     public boolean equals(Object o) {
@@ -22,18 +22,18 @@ public class SimpleOomage implements Oomage {
                 && (this.blue == other.blue);
     }
 
-    /* Uncomment this method after you've written
-       equals and failed the testHashCodeAndEqualsConsistency
-       test.
     @Override
     public int hashCode() {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
             // TODO: Write a perfect hash function for Simple Oomages.
-            return 0;
+            int hashCode = (blue * (256 * 256 ))
+                    + (green * 256)
+                    + (red * 1);
+            return hashCode;
         }
-    }*/
+    }
 
     public SimpleOomage(int r, int g, int b) {
         if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
--- hw3/src/test/java/com/icey/hw3/TestSimpleOomage.java
+++ hw3/src/test/java/com/icey/hw3/TestSimpleOomage.java
@@ -22,10 +22,18 @@ public class TestSimpleOomage {
 
     @Test
     public void testHashCodePerfect() {
-        /* TODO: Write a test that ensures the hashCode is perfect,
-          meaning no two SimpleOomages should EVER have the same
-          hashCode UNLESS they have the same red, blue, and green values!
-         */
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 256; i+=5) {
+            for (int j = 0; j < 256; j+=5) {
+                for (int k = 0; k < 256; k += 5) {
+                    SimpleOomage obj = new SimpleOomage(i, j, k);
+                    int hashcode = obj.hashCode();
+                    boolean hashCodePresent = set.contains(hashcode);
+                    assertFalse(hashCodePresent);
+                    set.add(hashcode);
+                }
+            }
+        }
     }
 
     @Test
@@ -39,7 +47,6 @@ public class TestSimpleOomage {
         assertNotEquals(ooA, "ketchup");
     }
 
-    /*
     @Test
     public void testHashCodeAndEqualsConsistency() {
         SimpleOomage ooA = new SimpleOomage(5, 10, 20);
@@ -47,7 +54,7 @@ public class TestSimpleOomage {
         HashSet<SimpleOomage> hashSet = new HashSet<>();
         hashSet.add(ooA);
         assertTrue(hashSet.contains(ooA2));
-    }*/
+    }
 
     /* TODO: Uncomment this test after you finish haveNiceHashCodeSpread in OomageTestUtility */
     /*@Test
